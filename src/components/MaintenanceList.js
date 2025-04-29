2a75eb7f4118f5c805bfc1021086b2840bc9192e
Vesa Kalliomäki <vesa.kalliomaki@student.careeria.fi>
2025-04-29 10:55:28 +0300

    modaali korjaus

--- src/components/MaintenanceList.js
+++ src/components/MaintenanceList.js
@@ -84,6 +84,16 @@ const MaintenanceList = () => {
   );
 };
 
+const modalStyle = {
+  position: "fixed",
+  top: 0, left: 0, right: 0, bottom: 0,
+  backgroundColor: "rgba(0,0,0,0.7)",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  zIndex: 1000,
+};
+
 const modalContentStyle = {
   background: "#fff",
   padding: "1rem",
